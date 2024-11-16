1efa93aa05470e663cac7a278df9da39b3e81d0f
Tichy958 <106491842+Tichy958@users.noreply.github.com>
2024-11-16 22:25:03 +0100

    everything copied from original

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".fa-solid").addEventListener("click", function () {
+  document.querySelector(".outer-grid").classList.toggle("active");
+});
+document.querySelector(".fa-solid").addEventListener("click", function () {
+  document.querySelector(".hide-nav").classList.toggle("active");
+});
